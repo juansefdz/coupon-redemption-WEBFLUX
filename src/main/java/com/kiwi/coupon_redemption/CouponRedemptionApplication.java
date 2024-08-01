173978cb0d75afb2d00968b7a7267e8fe6ae4442
juansefdz <jsfm81091@gmail.com>
2024-08-01 10:03:13 -0500

    project initialization

---
+++ src/main/java/com/kiwi/coupon_redemption/CouponRedemptionApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.coupon_redemption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponRedemptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponRedemptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiwi/coupon_redemption/CouponRedemptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.coupon_redemption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponRedemptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
