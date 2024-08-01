a062ac05701b9a2c6e138dd97f570b7b7a67044f
juansefdz <jsfm81091@gmail.com>
2024-08-01 10:46:35 -0500

    update

--- src/main/java/com/kiwi/coupon_redemption/config/SwaggerConfig.java
+++ src/main/java/com/kiwi/coupon_redemption/config/SwaggerConfig.java
@@ -3,8 +3,7 @@ package com.kiwi.coupon_redemption.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+
 
 @Configuration
 public class SwaggerConfig {
