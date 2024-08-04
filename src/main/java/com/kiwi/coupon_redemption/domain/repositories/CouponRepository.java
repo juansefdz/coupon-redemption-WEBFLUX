15b446301d13a79223bb6b7900b9bd435d79eda2
juansefdz <jsfm81091@gmail.com>
2024-08-04 09:51:48 -0500

    deleted project

--- src/main/java/com/kiwi/coupon_redemption/domain/repositories/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kiwi.coupon_redemption.domain.repositories;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import com.kiwi.coupon_redemption.domain.entities.Coupon;
-
-public interface CouponRepository extends ReactiveCrudRepository<Coupon, Long> {
-    
-}
